a5d4d41a1c7aec7c05a94587c9f9ef32af37d22b
Rosenthalk0 <rosenthalk0@gmail.com>
2022-02-05 03:13:08 -0600

    Updated Other Player Support

--- src/main/java/fun/tbcraft/play/commands/WorldCommand.java
+++ src/main/java/fun/tbcraft/play/commands/WorldCommand.java
@@ -1,7 +1,10 @@
 package fun.tbcraft.play.commands;
 
 import fun.tbcraft.play.utils.ColorWords;
+import org.apache.commons.lang3.Validate;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,6 +13,7 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
+import javax.xml.validation.Validator;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,25 +22,48 @@ public class WorldCommand implements CommandExecutor, TabExecutor{
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (label.equalsIgnoreCase("whereami") || label.equalsIgnoreCase("where")) {
-            if (!(sender instanceof Player)) {
+            if (!(sender instanceof Player send)) {
                 sender.sendMessage("Oops! This command can only be run by a player");
                 return true;
             }
-            Player player = (Player) sender;
+            if ( args.length == 1 ){
+                final var otherPlayer = Bukkit.getPlayerExact(args[0]);
+                final var nnPl = Validate.notNull(otherPlayer,"Bad Player Name");
 
-            String world = player.getWorld().getName();
-            int x = (int) (Math.floor(player.getLocation().getX()));
-            int y = (int) (Math.floor(player.getLocation().getX()));
-            int z = (int) (Math.floor(player.getLocation().getX()));
-            String coords = x + ", " + y + ", " + z;
+                if ( nnPl.isOnline() ){
+                    showLocation(nnPl, send);
+                }
+                else {
+                    OfflinePlayer offlinePlayer = nnPl;
+                    showLocation(otherPlayer, send);
 
-            player.sendMessage(ColorWords.get(
-                    "&3World&7: &7" + world));
-            player.sendMessage(ColorWords.get(
-                    "&3Coordinates&7: &7" + coords));
+                }
+            }
+            Player player = send;
+
+            showLocation(player);
             return true;
         }
         return false;
+    }
+
+    public void showLocation(Player player){
+        showLocation(player,null);
+    }
+    private void showLocation (Player player,@Nullable Player sender) {
+        String world = player.getWorld().getName();
+        int x = (int) (Math.floor(player.getLocation().getX()));
+        int y = (int) (Math.floor(player.getLocation().getX()));
+        int z = (int) (Math.floor(player.getLocation().getX()));
+        String coords = x + ", " + y + ", " + z;
+
+        if ( sender == null ){
+            sender = player;
+        }
+        sender.sendMessage(ColorWords.get(
+                "&3World&7: &7" + world));
+        sender.sendMessage(ColorWords.get(
+                "&3Coordinates&7: &7" + coords));
     }
 
     @Override
