2f270bbf5f6915c4476fd609b603ad8cfadd4435
Rosenthalk0 <rosenthalk0@gmail.com>
2022-02-10 14:53:09 -0600

    Added Resource Pack Event

--- src/main/java/fun/tbcraft/play/listener/StartupListener.java
+++ src/main/java/fun/tbcraft/play/listener/StartupListener.java
@@ -3,10 +3,14 @@ package fun.tbcraft.play.listener;
 import fun.tbcraft.play.TBCPlugin;
 import fun.tbcraft.play.player.TBCPlayer;
 import fun.tbcraft.play.utils.DisplayUtil;
+import fun.tbcraft.play.utils.TBCProperties;
 import net.Indyuce.mmocore.api.ConfigFile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerResourcePackStatusEvent;
 
+import java.security.MessageDigest;
+import java.security.MessageDigestSpi;
 import java.util.List;
 
 
@@ -17,7 +21,12 @@ public class StartupListener implements BaseListener{
     @EventHandler
     public void on(PlayerJoinEvent event){
         final var player = event.getPlayer();
-
+    }
+    @EventHandler
+    public void onResourcePack(PlayerResourcePackStatusEvent e){
+        if ( e.getStatus()== PlayerResourcePackStatusEvent.Status.DECLINED ) {
+            e.getPlayer().setResourcePack(TBCProperties.resourcePack, "",true);
+        }
     }
     @Override
     public String getName ( ) {
